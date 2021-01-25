4997751bc7fc1f4b33b17da10072c309fcef2906
onelivesleft <iainking@gmail.com>
2021-01-25 11:48:48 +0000

    Progress

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,10 @@
 # Change Log
 
-## [1.0.0] - 2021-
+## [0.0.4] - 2021-01-24
+* Mostly mostly grammared
+
+## [0.0.3] - 2021-01-24
+* Mostly grammared
+
+## [0.0.2] - 2021-01-23
 * First release.
--- src/extension.ts
+++ src/extension.ts
@@ -1,84 +1,62 @@
-'use strict';
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable curly */
+// TODO:
+// [ ] Goto definition
+// [ ] Find All References
+// [ ] Rename
+// [ ] Show current proc in status bar
+// [ ] Show #dump of current proc
+// [ ] Goto next error
+
+
 import * as vscode from 'vscode';
-import { commands, TextEditor, TextEditorEdit, Uri, ViewColumn } from 'vscode';
-import * as path from 'path';
-import * as fs from 'fs';
-
-
-const BOTH = 0;
-const UP   = 1;
-const DOWN = 2;
+import proc = require('child_process');
+import path = require('path');
+import fs = require('fs');
 
 
 export function activate(context: vscode.ExtensionContext) {
 
-  let blockMultiCursor = new BlockMultiCursor();
 
-  let growUp = vscode.commands.registerTextEditorCommand('blockMultiCursor.growUp', (editor: TextEditor, edit: TextEditorEdit) => {
-    blockMultiCursor.grow(UP, editor, edit);
-  });
+	let disposable = vscode.commands.registerCommand('the-language.test', () => {
+		jai([
+			"c:/repos/jai-cookbook/tools/locate.jai",
+			"--",
+			"c:/repos/jaitest/foo.jai",
+			"c:/repos/jaitest/foo.jai",
+			"213", "5",
+		]).then(output => {
+			if (output === undefined)  return;
+			console.log(output);
+		});
+	});
 
-  let growDown = vscode.commands.registerTextEditorCommand('blockMultiCursor.growDown', (editor: TextEditor, edit: TextEditorEdit) => {
-    blockMultiCursor.grow(DOWN, editor, edit);
-  });
+	context.subscriptions.push(disposable);
+}
 
-  let grow = vscode.commands.registerTextEditorCommand('blockMultiCursor.grow', (editor: TextEditor, edit: TextEditorEdit) => {
-    blockMultiCursor.grow(BOTH, editor, edit);
-  });
+export function deactivate() {}
 
 
-  context.subscriptions.push(growUp);
-  context.subscriptions.push(growDown);
-  context.subscriptions.push(grow);
+async function jai(args: string[]): Promise<string | undefined> {
+	let config = vscode.workspace.getConfiguration('the-language');
+	let exe_path = config.get("pathToJaiExecutable");
+	if (exe_path === undefined) return;
+
+	return execCommand(exe_path as string, args);
 }
 
 
-export function deactivate() {
-}
-
-
-class BlockMultiCursor {
-  async grow(direction: number, editor: TextEditor, edit: TextEditorEdit) {
-    let sortedSelections = editor.selections.sort((a, b) => (a.end.line - b.end.line));
-
-    if (direction === BOTH || direction === UP) {
-      let cursor = sortedSelections[0];
-      let column = cursor.end.character;
-      let startLine = cursor.end.line - 1;
-      if (startLine > 0 && direction === UP) {
-        let lineText = editor.document.lineAt(startLine).text;
-        while (startLine > 0 && lineText.length < column) {
-          startLine--;
-          lineText = editor.document.lineAt(startLine).text;
-        }
-      }
-      for (let line = startLine; line > 0; line--) {
-        let lineText = editor.document.lineAt(line).text;
-        if (lineText.length < column) break;
-
-        editor.selections.push(new vscode.Selection(line, column, line, column));
-      }
-    }
-
-    if (direction === BOTH || direction === DOWN) {
-      let cursor = sortedSelections[sortedSelections.length - 1];
-      let column = cursor.end.character;
-      let startLine = cursor.end.line + 1;
-      if (startLine < editor.document.lineCount && direction === DOWN) {
-        let lineText = editor.document.lineAt(startLine).text;
-        while (startLine < editor.document.lineCount - 1 && lineText.length < column) {
-          startLine++;
-          lineText = editor.document.lineAt(startLine).text;
-        }
-      }
-      for (let line = startLine; line < editor.document.lineCount; line++) {
-        let lineText = editor.document.lineAt(line).text;
-        if (lineText.length < column) break;
-
-        editor.selections.push(new vscode.Selection(line, column, line, column));
-      }
-    }
-
-    editor.selections = editor.selections;
-  }
+function execCommand(exe_path: string, args: string[]): Promise<string | undefined> {
+	return new Promise((resolve, reject) => {
+		proc.execFile(exe_path, args, (error, stdout, stderr) => {
+			if (error) {
+				console.log(error);
+				console.log(stderr);
+				reject(error);
+			}
+			else {
+				resolve(stdout);
+			}
+		});
+	});
 }
